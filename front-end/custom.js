a9bf301cadec128de4900bd81c1925376e5672df
Thinking In Digital <35577764+thinkingindigital@users.noreply.github.com>
2022-02-10 13:14:18 -0600

    Delete front-end directory

--- front-end/custom.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-
-    $('.form-check').on('click', function() {
-        $('.form-check').removeClass('form-check--active');
-        $(this).addClass('form-check--active');
-    });
-
-});
